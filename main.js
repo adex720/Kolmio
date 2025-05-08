367c9d38ad14197653934a4095d3cdeb5312d5ce
adex720 <25402611+adex720@users.noreply.github.com>
2025-05-08 10:22:22 +0300

    Make checkbox slider show/hide rest of page

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+function showAdvanced(){
+    var checkBox = document.getElementById("show-advanced");
+
+    document.getElementById("image-creation").style.display = checkBox.checked ? "inline" : "none";
+}
