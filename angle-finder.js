dad4d1e6d2f099d74e81f36fc9cd75b2bc0a5467
adex720 <25402611+adex720@users.noreply.github.com>
2025-05-08 14:34:35 +0300

    Allow finding multiple triangles

--- angle-finder.js
+++ angle-finder.js
@@ -1,5 +1,7 @@
 
-function calculate(angle, digits) {
+function calculate(angle, digits, amount=1) {
+    let result = [];
+
     let tan = Math.tan(angle * Math.PI / 180);
 
     let m = 2;
@@ -18,13 +20,30 @@ function calculate(angle, digits) {
 
         let diff = sin - angle;
 
-        if (0 < diff && diff < 1 && zeroCount(diff) >= digits) break;
+        if (0 < diff && diff < 1 && zeroCount(diff) >= digits){
+            let d = gcd(gcd(a, b), c);
+            let newC = c / d;
+
+            let valid = true;
+            for (let previous of result) {
+                if (previous[2] == newC) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                result.push([a / d, b / d, newC]);
+
+                amount--;
+                if (amount == 0) break;
+            }
+        }
 
         if (v > tan || n + 1 >= m) m++;
         else n++;
     }
 
-    let d = gcd(gcd(a, b), c);
-
-    return [a / d, b / d, c / d];
+    result.sort((a, b) => a[2] - b[2]);
+    return result;
 }
--- main.js
+++ main.js
@@ -6,23 +6,66 @@ function displayError(message) {
 function updateTriangle() {
     let angle = Number(document.getElementById("angle").value);
     let decimals = Number(document.getElementById("decimals").value);
+    let amount = Number(document.getElementById("triangles").value);
 
-    if (angle == 0 || decimals == 0) {
+    if (angle == 0 || decimals == 0 || amount == 0) {
         displayError("Virheellinen syöte: 0");
         return;
     }
 
-    let result = calculate(angle, decimals);
-    let a = result[0];
-    let b = result[1];
-    let c = result[2];
+    setResultElementCount(amount);
 
-    let sin = (Math.asin(b / c) * 180 / Math.PI).toFixed(decimals + 3);
+    let results = calculate(angle, decimals, amount);
 
-    document.getElementById("side-a").innerHTML = "a = " + a;
-    document.getElementById("side-b").innerHTML = "b = " + b;
-    document.getElementById("side-c").innerHTML = "c = " + c;
-    document.getElementById("sin").innerHTML = "sin = " + sin + "&deg;";
+    for (let i = 0; i < amount; i++) {
+    let result = results[i];
+        let a = result[0];
+        let b = result[1];
+        let c = result[2];
+
+        let sin = (Math.asin(b / c) * 180 / Math.PI).toFixed(decimals + 3);
+
+        document.getElementById("side-a" + i).innerHTML = "a = " + a;
+        document.getElementById("side-b" + i).innerHTML = "b = " + b;
+        document.getElementById("side-c" + i).innerHTML = "c = " + c;
+        document.getElementById("sin" + i).innerHTML = "sin = " + sin + "&deg;";
+    }
+}
+
+function setResultElementCount(n) {
+    let elements = document.getElementsByClassName("result-values");
+    let current = elements.length;
+    for (let i = n; i < current; current--)
+        elements[i].remove();
+
+    let parent = document.getElementById("results");
+    for (let i = current; i < n; i++){
+        let div = document.createElement("div");
+        div.id = "t" + i;
+        div.classList.add("result-values");
+
+        let pa = document.createElement("p");
+        pa.id = "side-a" + i;
+        pa.classList.add("result-value");
+        div.appendChild(pa);
+
+        let pb = document.createElement("p");
+        pb.id = "side-b" + i;
+        pb.classList.add("result-value");
+        div.appendChild(pb);
+
+        let pc = document.createElement("p");
+        pc.id = "side-c" + i;
+        pc.classList.add("result-value");
+        div.appendChild(pc);
+
+        let ps = document.createElement("p");
+        ps.id = "sin" + i;
+        ps.classList.add("result-value");
+        div.appendChild(ps);
+
+        parent.appendChild(div);
+    }
 }
 
 function showAdvanced() {
